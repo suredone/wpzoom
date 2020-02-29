757d315a90637591958ef3614811757bff8df60b
Casey Milne <joel@goldhat.ca>
2020-02-29 00:51:52 -0600

    draft zoom calendar shortcode

---
+++ src/shortcodes/zoom_calendar/shortcode.php
@@ -0,0 +1,44 @@
+<?php
+
+class WPZOOM_ZoomCalendarShortcode {
+
+  public $tag = 'zoom_calendar';
+
+  public function __construct() {
+    add_action('init', array( $this, 'init'));
+  }
+
+  public function init() {
+    add_shortcode($this->tag, array($this, 'doShortcode'));
+  }
+
+  public function doShortcode( $atts ) {
+
+    $atts = shortcode_atts( array(
+      'foo' => 'no foo',
+      'baz' => 'default baz'
+    ), $atts, 'bartag' );
+
+    $key    = 'MzjAyttgT76CM79z47S1kA';
+    $secret = 'TZLZVlQnoo0APtFaaTYfFb4UudC4EgYL3AoR';
+    $zoomUsers = new Zoom\Endpoint\Users( $key, $secret );
+    $userResponse = $zoomUsers->list();
+    $userFirst = $userResponse['users'][0];
+
+
+    $zoomWebinar = new Zoom\Endpoint\Webinar( $key, $secret );
+    $webinarResponse = $zoomWebinar->list( $userFirst['id'], [] );
+
+    /* test templating */
+    $template = new WPZOOM_Template();
+    $template->templatePath = 'src/shortcodes/zoom_calendar/templates/';
+    $template->templateName = 'table';
+    $template->data = array(
+      'webinarResponse' => $webinarResponse,
+      'webinars' => $webinarResponse['webinars']
+    );
+    return $template->get();
+
+  }
+
+}
