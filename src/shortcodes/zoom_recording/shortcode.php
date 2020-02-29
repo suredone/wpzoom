daf4b5345fbf4ed36202c4160a112e2966c61f3a
Casey Milne <joel@goldhat.ca>
2020-02-29 01:35:46 -0600

    draft recording shortcode

---
+++ src/shortcodes/zoom_recording/shortcode.php
@@ -0,0 +1,43 @@
+<?php
+
+class WPZOOM_ZoomRecordingShortcode {
+
+  public $tag = 'zoom_recording';
+
+  public function __construct() {
+    add_action('init', array( $this, 'init'));
+  }
+
+  public function init() {
+    add_shortcode($this->tag, array($this, 'doShortcode'));
+  }
+
+  public function doShortcode( $atts ) {
+
+    $atts = shortcode_atts( array(
+      'foo' => 'no foo',
+      'baz' => 'default baz'
+    ), $atts, 'bartag' );
+
+    $key    = 'MzjAyttgT76CM79z47S1kA';
+    $secret = 'TZLZVlQnoo0APtFaaTYfFb4UudC4EgYL3AoR';
+    $zoomUsers = new Zoom\Endpoint\Users( $key, $secret );
+    $userResponse = $zoomUsers->list();
+    $userFirst = $userResponse['users'][0];
+
+
+    $zoomRecording = new Zoom\Endpoint\Recording( $key, $secret );
+    $recordingResponse = $zoomRecording->list( $userFirst['id'], [] );
+
+    $template = new WPZOOM_Template();
+    $template->templatePath = 'src/shortcodes/zoom_recording/templates/';
+    $template->templateName = 'table';
+    $template->data = array(
+      'response' => $recordingResponse,
+      'recordings' => $recordingResponse['recordings']
+    );
+    return $template->get();
+
+  }
+
+}
---
+++ src/shortcodes/zoom_recording/templates/table.php
@@ -0,0 +1,38 @@
+<?php
+
+print '<pre>';
+var_dump( $recordingResponse );
+print '</pre>';
+
+?>
+
+<h1>Upcoming Webinars</h1>
+
+<table>
+  <thead>
+    <tr>
+      <th>Title</th>
+      <th>Start</th>
+      <th>Duration</th>
+      <th>&nbsp;</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach( $webinars as $webinar ) : ?>
+      <tr>
+        <td>
+          <?php print $webinar['topic']; ?>
+        </td>
+        <td>
+          <?php print $webinar['start_time']; ?>
+        </td>
+        <td>
+          <?php print $webinar['duration']; ?> Minutes
+        </td>
+        <td>
+          <button>Register</button>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
